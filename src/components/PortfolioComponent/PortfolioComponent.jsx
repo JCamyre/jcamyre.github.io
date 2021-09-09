ca3713f915bbdf59f33f04229e8653bd8df71c9f
JCamyre <JWcamry03@gmail.com>
2021-09-09 12:49:35 -0700

    Add css and components to Portfolio page

--- src/components/PortfolioComponent/PortfolioComponent.jsx
+++ src/components/PortfolioComponent/PortfolioComponent.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { Button1 } from './../Button1/Button1';
+import './PortfolioComponent.css';
 
 function PortfolioComponent(props) {
     return (
         //  Put github url here?
+        // Different states when you hover over
         <Link to={props.url}>
-            <div>
+            <div className='portfolio-component'>
                 <div>
                     <img alt='thumbnail for project' src={props.thumbnail} />
                 </div>
@@ -15,7 +18,7 @@ function PortfolioComponent(props) {
                     </div>
                     <h2>{props.title}</h2>
                     <h4>{props.description}</h4>
-                    <h4>View <div className='button'></div></h4>
+                    <h4><Button1>View</Button1></h4>
                 </div>
             </div>
         </Link>
