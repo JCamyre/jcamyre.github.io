4ad8565559d474b49ddc5f083d5a4f340d76940c
JCamyre <JWcamry03@gmail.com>
2021-09-08 23:13:59 -0700

    Add PortfolioComponent

--- src/components/pages/Portfolio/Portfolio.jsx
+++ src/components/pages/Portfolio/Portfolio.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function Portfolio() {
     return (
         <div>
-            
+            {/* Hover over div to zoom a lil (both the pic/thumbnail and actual component), picture, subtitle, title, description/about, view, have a thing to sort type/category by */}
+
         </div>
     )
 }
---
+++ src/components/pages/PortfolioComponent/PortfolioComponent.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+function PortfolioComponent(props) {
+    return (
+        //  Put github url here?
+        <Link to={props.url}>
+            <div>
+                <div>
+                    <img src={props.thumbnail} />
+                </div>
+                <div>
+                    <div className='button'>
+                        <h5>{props.category}</h5>
+                    </div>
+                    <h2>{props.title}</h2>
+                    <h4>{props.description}</h4>
+                    <h4>View <div className='button'></div></h4>
+                </div>
+            </div>
+        </Link>
+    )
+}
+
+export default PortfolioComponent
