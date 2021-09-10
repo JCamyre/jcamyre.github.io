690cda80cb808589baf26a495d49f65f2aa3718d
JCamyre <JWcamry03@gmail.com>
2021-09-10 00:10:59 -0700

    Add footer component and refactor code for footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SocialMediaButton from './../SocialMediaButton/SocialMediaButton';
+
+function Footer() {
+    return (
+        <footer>
+            
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/SocialMediaButton/SocialMediaButton.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function SocialMediaButton(props) {
+    return (
+        <div>
+            <a
+              href="https://www.instagram.com/realmizkif/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                alt="Instagram logo"
+                className="social-media-icons instagram"
+                src="https://uploads-ssl.webflow.com/5fdcc89b08dd76ebd582ce20/5fdcc89b1788e769745cfdc8_icon-social-media-03-designer-template.svg"
+              />
+            </a>
+        </div>
+    )
+}
+
+export default SocialMediaButton
